fbf89305875359597b55397242f78b552b7af3c8
ZafarKamal123 <sk4915497@gmail.com>
2021-12-27 16:17:31 +0500

    :sparkles: Initial Commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const { convert } = require('convert-svg-to-png');
+
+const fs = require("fs");
+
+const PORT = 3000;
+
+
+function replacePlaceholders(template, query) {
+
+  for (const q in query) {
+
+    const isPlaceholder = q.startsWith('{{') && q.endsWith('}}');
+
+    if (!isPlaceholder) continue;
+
+    template = template.split(q).join(query[q]);
+
+  }
+
+  return template;
+}
+
+app.get('/', function (request, response) {
+
+  const requestedTemplate = request.query?.template ?? null;
+
+  if (!requestedTemplate) {
+    return response.status(404).json({ error: 'Template not found!' });
+  }
+
+  let templatePath = './templates/' + request.query.template + '.svg';
+
+  if (!fs.existsSync(templatePath)) {
+    return response.status(404).json({ error: 'Template not found!' });
+  }
+
+  let svgTemplate = fs.readFileSync(templatePath, { encoding: 'utf-8' });
+
+  svgTemplate = replacePlaceholders(svgTemplate, request.query);
+
+  return convert(svgTemplate).then(image => {
+    response.writeHead(200, {
+      'Content-Type': 'image/jpg',
+      'Content-Length': image.length
+    });
+    response.end(image);
+  });
+
+});
+
+app.listen(PORT, () => {
+  console.log('Server listening on http://localhost:' + PORT);
+})
